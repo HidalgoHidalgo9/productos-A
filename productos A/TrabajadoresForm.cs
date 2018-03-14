35b3478e92598e284088af1513b8de6cafbe9d49
HidalgoHidalgo9 <adrenalina_hector@hotmail.com>
2018-03-14 13:20:25 -0600

    se agrega froms a tabla Trabajadores con nombre y clave

---
+++ productos A/TrabajadoresForm.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace productos_A
+{
+    public partial class TrabajadoresForm : Form
+    {
+        public TrabajadoresForm()
+        {
+            InitializeComponent();
+        }
+
+        private void trabajadoresBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.trabajadoresBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.postgresDataSet);
+
+        }
+
+        private void TrabajadoresForm_Load(object sender, EventArgs e)
+        {
+            // TODO: esta línea de código carga datos en la tabla 'postgresDataSet.trabajadores' Puede moverla o quitarla según sea necesario.
+            this.trabajadoresTableAdapter.Fill(this.postgresDataSet.trabajadores);
+
+        }
+
+        private void nombreTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
